d8c95587ffaae3f155d596b747ae6dc9dda39e94
Chenuka Medawela <52198262+ChenukaMedawela@users.noreply.github.com>
2020-06-09 21:03:53 +0530

    Add files via upload

---
+++ Assignment3/EvenOdd4.scala
@@ -0,0 +1,35 @@
+ import scala.io.StdIn.readInt
+
+  object EvenOdd4 {
+
+    def isEven(numb:Int):Boolean= numb match{
+      case 0 => true;
+      case _ => isOdd(numb-1);
+
+    }
+
+    def isOdd(numb:Int):Boolean = !(isEven(numb));
+
+    def checker(numb:Int):Int={
+      println("Insert the Number");
+      var numb:Int=readInt();
+
+      if(isOdd(numb)){
+        println(numb+" is a Odd number");
+
+      }
+      else
+      {
+        println(numb+" is a even number");
+      }
+      checker(1);
+    }
+
+
+    def main(args:Array[String]){
+      checker(1);
+
+    }
+  }
+
+
---
+++ Assignment3/Fibonacci6.scala
@@ -0,0 +1,23 @@
+import scala.io.StdIn.readInt;
+
+object Fibonacci6 {
+
+  def fib(numb:Int):Int = numb match{
+    case 0 => 0;
+    case 1 => 1;
+    case _ => fib(numb-1) + fib(numb-2);
+  }
+
+  def fibPrint(numb:Int):Unit = {
+    if(numb > 0) fibPrint(numb-1)
+    print(fib(numb)+"\t")
+  }
+
+  def main(args:Array[String]){
+    println("Enter the number of element wants in series: ");
+    val numb:Int=readInt();
+
+    fibPrint(numb);
+
+  }
+}
